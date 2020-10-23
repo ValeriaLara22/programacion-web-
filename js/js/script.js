80d262af482cbcb931530d9cbc9bf70ff5bdb3ac
ValeriaLara22 <L17111911@cdjuarez.tecnm.mx>
2020-10-23 08:19:51 -0500

    23-10-2020

---
+++ js/js/script.js
@@ -0,0 +1,18 @@
+console.log(5);
+console.log("Hola Mundo!".length);
+console.log(234.456);
+console.log(2+3);
+console.log(34-12);
+
+//tipos de datos: numericos, string, boolean, null, undifined,symbol, object
+//variables
+
+var x=10;
+x = 'Hola';
+let y=15;
+const vacio = false;
+
+//ejemplo split
+const otraCadena = 'Hola Mundo';
+console.log(otraCadena.match('Hola'));
+console.log(otraCadena.match('Reloading'));
